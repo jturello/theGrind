be1689b73f27717c50ac8c9551d9596f7cc94d34
James Turello <jdturello@gmail.com>
2016-02-02 09:52:49 -0800

    add code and passing specs for Post.prototype function edtiPost allowing an admin to update a post's properties

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ function Post(title, subtext, img_url, type, post_body) {
   this.post_body = post_body;
 }
 
+Post.prototype.editPost = function(newTitle, newSubtext, new_img_url, newType, new_post_body) {
+  this.title = newTitle;
+  this.subtext = newSubtext;
+  this.img_url = new_img_url;
+  this.type = newType;
+  this.post_body = new_post_body;
+}
 
 
 function Admin(userName, password){
